dd32b5e1a5f7ee78c948c20806055341a2a30c8e
Reza <31606295+reza-mesgaran@users.noreply.github.com>
2024-04-18 15:17:12 -0400

    Add C# file

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace sampleConsoleApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("This is a smple console appl;ication written by .NET framework 4.7");
+        }
+    }
+}
